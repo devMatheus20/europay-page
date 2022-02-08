a849d12611d01060035c98186f77c16df138193e
Matheus Santos <matheussantos77@outlook.com>
2022-02-08 12:13:38 -0300

    page complete

--- assets/index.js
+++ assets/index.js
@@ -5,6 +5,6 @@ function showList (){
     if(menuList.style.maxHeight == "0px") {
         menuList.style.maxHeight = "300px"
     } else {
-        menuList.style.maxHeight = "0px"
+        menuList.style.maxHeight ="0px"
     }
 }
